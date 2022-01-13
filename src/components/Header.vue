f1fdb994472b51b48a4a951b1b7fd44f3f486ec8
Filadelfio Granata <philadelphos.granata@gmail.com>
2022-01-13 17:09:38 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <img src="./../assets/img/dc-logo.png" />
+    <ul>
+      <li>
+        <a>menu-item</a>
+      </li>
+      <li>
+        <a>menu-item</a>
+      </li>
+      <li>
+        <a>menu-item</a>
+      </li>
+      <li>
+        <a>menu-item</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  background-color: #ffffff;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+img {
+  height: 80%
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
